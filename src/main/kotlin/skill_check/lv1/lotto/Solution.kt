3c22727d7a8d88c35c077496395e569b4bc67dc6
ok0 <ongnya@gmail.com>
2022-07-29 21:46:29 +0900

    first commit

---
+++ src/main/kotlin/skill_check/lv1/lotto/Solution.kt
@@ -0,0 +1,17 @@
+package skill_check.lv1.lotto
+
+/*
+* https://school.programmers.co.kr/learn/courses/30/lessons/77484#fn1
+*
+* */
+class Solution {
+  fun solution(lottos: IntArray, win_nums: IntArray): IntArray {
+    fun rank(matched: Int) = if (matched < 2) 6 else 7 - matched
+
+    val filterd = lottos.filter { it != 0 }.toSet()
+    val matched = win_nums.filter { filterd.contains(it) }.size
+    val matchedMax = matched + (win_nums.size - filterd.size)
+
+    return intArrayOf(rank(matchedMax), rank(matched))
+  }
+}
---
+++ src/test/kotlin/skill_check/lv1/bad_user_reporter/SolutionTest.kt
@@ -0,0 +1,23 @@
+package skill_check.lv1.bad_user_reporter
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.util.*
+
+class SolutionTest {
+  @Test
+  fun solutionTest() {
+    val s = Solution()
+
+    val id = listOf<String>("muzi", "frodo", "apeach", "neo")
+    val report = listOf<String>("muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi")
+    val s1 = s.solution(
+      id_list = id.toTypedArray(),
+      report = report.toTypedArray(),
+      k = 2
+    )
+    val a1 = listOf(2,1,1,0)
+
+    Assertions.assertEquals(s1.contentToString(), a1.toString())
+  }
+}
