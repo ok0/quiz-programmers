aa59f49e4abad9b6ce960d90b0d7fc12a10200ac
ok0 <ongnya@gmail.com>
2022-07-31 19:09:12 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/81301

---
+++ src/main/kotlin/skill_check/lv1/convert_string_to_digit/Solution.kt
@@ -0,0 +1,40 @@
+package skill_check.lv1.convert_string_to_digit
+
+/*
+* https://school.programmers.co.kr/learn/courses/30/lessons/81301
+*
+* */
+class Solution {
+  companion object {
+    val stringToDigit = mapOf(
+      "zero" to "0",
+      "one" to "1",
+      "two" to "2",
+      "three" to "3",
+      "four" to "4",
+      "five" to "5",
+      "six" to "6",
+      "seven" to "7",
+      "eight" to "8",
+      "nine" to "9"
+    )
+  }
+
+  fun solution(s: String): Int {
+    var answer = StringBuilder()
+    val word = StringBuilder()
+    s.forEach { c ->
+      if (c.isDigit()) {
+        answer.append(c)
+      } else {
+        word.append(c)
+        stringToDigit[word.toString()]?.let { w ->
+          answer.append(w)
+          word.clear()
+        }
+      }
+    }
+
+    return answer.toString().toInt()
+  }
+}
