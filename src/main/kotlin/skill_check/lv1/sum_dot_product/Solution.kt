902d0f998907eed31ea21a8ff75211d1eb175bff
ok0 <ongnya@gmail.com>
2022-08-02 16:58:43 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/70128

---
+++ src/main/kotlin/skill_check/lv1/sum_dot_product/Solution.kt
@@ -0,0 +1,14 @@
+package skill_check.lv1.sum_dot_product
+
+/**
+ *
+ * https://school.programmers.co.kr/learn/courses/30/lessons/70128
+ *
+ * */
+class Solution {
+  fun solution(a: IntArray, b: IntArray): Int {
+    return a.mapIndexed { idx, n ->
+      n * b[idx]
+    }.sum()
+  }
+}
