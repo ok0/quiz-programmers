4638d4615e647d8dae1a03243863242e4f2bb6ca
ok0 <ongnya@gmail.com>
2022-08-02 16:24:04 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/64061

---
+++ src/main/kotlin/skill_check/lv1/claw_crane/Solution.kt
@@ -0,0 +1,40 @@
+package skill_check.lv1.claw_crane
+
+import java.util.*
+
+/*
+* https://school.programmers.co.kr/learn/courses/30/lessons/64061
+*
+* */
+class Solution {
+  fun solution(board: Array<IntArray>, moves: IntArray): Int {
+    var answer = 0
+
+    val width = board[0].lastIndex
+    val stack = Stack<Int>()
+
+    moves.forEach { n ->
+      val position = (n - 1)
+      var last = 0
+      var isPicked = false
+      while (!isPicked && last <= width) {
+        val picked = board[last][position]
+        if (picked != 0) {
+          if (!stack.isEmpty() && stack.peek() == picked) {
+            answer += 2
+            stack.pop()
+          } else {
+            stack.add(picked)
+          }
+
+          board[last][position] = 0
+          isPicked = true
+        }
+
+        last++
+      }
+    }
+
+    return answer
+  }
+}
